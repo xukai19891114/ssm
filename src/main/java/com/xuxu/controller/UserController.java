d0c3adede5f199344ce847dc2f0b4c9151ba2e4f
xuxuxu <270692929@qq.com>
2018-12-11 10:56:10 +0800

    添加controller 增删改查方法

--- src/main/java/com/xuxu/controller/UserController.java
+++ src/main/java/com/xuxu/controller/UserController.java
@@ -14,6 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.xuxu.entity.UserEntity;
 import com.xuxu.service.UserService;
@@ -41,10 +44,67 @@ public class UserController{
 		return userService.save(user);
 	}
 	
-	@RequestMapping(value="/users",method=RequestMethod.POST)
-	public boolean insertb(@RequestBody String usersStr){
+	/*
+	 * 批量添加
+	 * 前端传入JSON对象数组字符串 后端使用字符串接收
+	 * Content-Type=application/json
+	 * [{"username":"t1"},{"username":"t2"},{"username":"t3"}]
+	 */
+	@RequestMapping(value="/users/str",method=RequestMethod.POST)
+	public boolean insertBatchByStr(@RequestBody String usersStr){
 		JSONArray users = JSONObject.parseArray(usersStr);
 		List<UserEntity> userList =users.toJavaList(UserEntity.class);
 		return userService.saveBatch(userList);
+	}
+	
+	/*
+	 * 批量添加
+	 * 前端传入JSON对象数组字符串
+	 * Content-Type=application/json
+	 * [{"username":"t1"},{"username":"t2"},{"username":"t3"}]
+	 */
+	@RequestMapping(value="/users/list",method=RequestMethod.POST)
+	public boolean insertBatchByList(@RequestBody List<UserEntity> users){
+		return userService.saveBatch(users);
+	}
+	
+	/*
+	 * 存在更新记录，否插入一条记录 
+	 * {"id":113,"username":"saveOrUpdate"}
+	 */
+	@RequestMapping(value="/users/saveOrUpdate",method=RequestMethod.POST)
+	public void saveOrUpdate(@RequestBody UserEntity user){
+		userService.saveOrUpdate(user);
+	}
+	
+	/*
+	 * 根据条件修改
+	 * {"username":"updateTest"}
+	 */
+	@RequestMapping(value="/users",method=RequestMethod.PUT)
+	public void update(@RequestBody UserEntity user){
+		UpdateWrapper<UserEntity> updateWrapper = new UpdateWrapper<>();
+		updateWrapper.eq("username", "t1");
+		userService.update(user, updateWrapper);
+	}
+	
+	/*
+	 *根据ID批量删除
+	 *[1,2,3,4] 
+	 */
+	@RequestMapping(value="/users",method=RequestMethod.DELETE)
+	public void delete(@RequestBody List<Integer> idList){
+		userService.removeByIds(idList);
+	}
+	
+	/*
+	 * 根据条件查询
+	 * 
+	 */
+	@RequestMapping(value="/users",method=RequestMethod.GET)
+	public List<UserEntity> query(){
+		QueryWrapper<UserEntity> queryWrapper = new QueryWrapper<>();
+		queryWrapper.like(true, "username", "xu");
+		return userService.list(queryWrapper);
 	}
 }
